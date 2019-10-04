b3a1f742a91d255873535fd96eec2207cd8ba3e8
ZvegintsevRoman <zvegintsev.roma@yandex.by>
2019-10-04 18:41:34 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  setRange(min, max) {
+    this._min = min;
+    this._max = max;  
+  }
 
-    }
+  guess() {
+    this._current = Math.ceil((this._max + this._min) / 2);
+    return this._current;
+  }
 
-    guess() {
+  lower() {
+    this._max = this._current;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this._min = this._current;
+  }
 }
 
 module.exports = GuessingGame;
